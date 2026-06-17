c0761e09f40d130de8d34c627332b1f8e971a3e8
Ayaanyx7 <ayaanyx7@gmail.com>
2026-06-17 16:14:44 +0100

    Refactor SRB2Game class to HIDDevice interface

--- android/app/src/main/java/org/libsdl/app/HIDDevice.java
+++ android/app/src/main/java/org/libsdl/app/HIDDevice.java
@@ -1,34 +1,22 @@
-package org.kartkrew.srb2k;
+package org.libsdl.app;
 
-import org.libsdl.app.SDLActivity;
+import android.hardware.usb.UsbDevice;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-
-public class SRB2Game extends SDLActivity {
-	public static boolean checkPermission(String permission) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-			return true;
-		}
-		Activity activity = (Activity)getContext();
-		if (activity.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
-			return true;
-		return false;
-	}
-
-
-	public static boolean inMultiWindowMode() {
-		if (Build.VERSION.SDK_INT >= 24) {
-			if (SRB2Game.mSingleton.isInMultiWindowMode())
-				return true;
-		}
-		return false;
-	}
-	public static void openFolder()
-	{
-		Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-	}
+interface HIDDevice
+{
+    public int getId();
+    public int getVendorId();
+    public int getProductId();
+    public String getSerialNumber();
+    public int getVersion();
+    public String getManufacturerName();
+    public String getProductName();
+    public UsbDevice getDevice();
+    public boolean open();
+    public int sendFeatureReport(byte[] report);
+    public int sendOutputReport(byte[] report);
+    public boolean getFeatureReport(byte[] report);
+    public void setFrozen(boolean frozen);
+    public void close();
+    public void shutdown();
 }
